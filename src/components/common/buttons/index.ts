f9446ae4718af51efe9d92e4ad48578509c52c8d
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-02-17 13:20:25 +0200

    fix: fixing naming of the files

--- src/components/common/buttons/index.ts
+++
@@ -1,6 +0,0 @@
-export { NumberButton } from './numberButton';
-export { ChevronButton } from './chevronButton';
-export { RoundButton } from './roundButton';
-export { HeartButton } from './heartButton';
-export { PrimaryButton } from './primaryButton';
-export { ArrowButton } from './arrowButton';
