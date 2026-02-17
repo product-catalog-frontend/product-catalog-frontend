a0ef5242cb13ba7df1c6cbd27fffa8ed6b797c6c
Adelina Yovdii <adelincaiovdii@gmail.com>
2026-02-17 12:58:39 +0200

    refactor: file names

--- src/components/common/buttons/arrowButton.tsx
+++ src/components/common/buttons/arrowButton.tsx
@@ -1,5 +1,5 @@
 import { cn } from '../../../lib/utils';
-import { Icon } from '../../common/Icon';
+import { Icon } from '../Icon';
 
 interface ArrowButtonProps {
   text?: string;
--- src/components/common/buttons/index.ts
+++ src/components/common/buttons/index.ts
@@ -1,6 +1,6 @@
-export { NumberButton } from './numberButton';
-export { ChevronButton } from './chevronButton';
-export { RoundButton } from './roundButton';
-export { HeartButton } from './heartButton';
-export { PrimaryButton } from './primaryButton';
-export { ArrowButton } from './arrowButton';
+export { NumberButton } from './NumberButton';
+export { ChevronButton } from './ChevronButton';
+export { RoundButton } from './RoundButton';
+export { HeartButton } from './HeartButton';
+export { PrimaryButton } from './PrimaryButton';
+export { ArrowButton } from './ArrowButton';
