b81fc49c44a9890e903e4b72084d995428ede3cf
dmytro-halieba <dmytro.halyeba@gmail.com>
2026-02-17 13:29:20 +0200

    Delete src/components/common/buttons directory

--- src/components/common/buttons/index.ts
+++
@@ -1,6 +0,0 @@
-export { NumberButton } from './NumberButton';
-export { ChevronButton } from './ChevronButton';
-export { RoundButton } from './RoundButton';
-export { HeartButton } from './HeartButton';
-export { PrimaryButton } from './PrimaryButton';
-export { ArrowButton } from './ArrowButton';
