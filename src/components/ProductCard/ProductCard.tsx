3548a8274edd737330850bd1ce76010dc0e0316c
Dmytro Kerziuk <kerzyukd@mail.com>
2026-02-17 16:14:28 +0200

    feat two buttons with styles

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -5,10 +5,17 @@ import { HeartButton, PrimaryButton } from '../common/Buttons';
 
 interface ProductCardProps {
   product: Product;
+  isFavorite?: boolean;
+  onFavoriteClick?: () => void;
 }
 
-export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+export const ProductCard: React.FC<ProductCardProps> = ({
+  product,
+  isFavorite = false,
+  onFavoriteClick,
+}) => {
   const { image, name, price, screen, capacity, ram } = product;
+
   return (
     <article className={styles.card}>
       <a
@@ -42,7 +49,10 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           label="Add to cart"
           selectedLabel="Added"
         />
-        <HeartButton />
+        <HeartButton
+          selected={isFavorite}
+          onClick={onFavoriteClick}
+        />
       </div>
     </article>
   );
